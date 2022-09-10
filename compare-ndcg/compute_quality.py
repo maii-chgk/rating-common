4179f5b746bb4f8de169424947f7a8f7bacafe15
Jury Razumau <jury.razumau@zendesk.com>
2022-09-10 17:53:40 +0200

    refactor ndcg into functions

--- compare-ndcg/compute_quality.py
+++ compare-ndcg/compute_quality.py
@@ -9,14 +9,65 @@ import scipy.stats as stats
 import sklearn.metrics as metrics
 import warnings
 
+
+def db_connection():
+    conn_str = os.environ.get('DATABASE')
+    if conn_str is None:
+        raise Exception('DATABASE is not defined')
+    return psycopg2.connect(conn_str)
+
+
+def fetch_tournaments(connection) -> list:
+    log.info('Fetching all tournaments')
+    tournaments_query = f'''
+        SELECT id 
+        FROM tournaments 
+        WHERE maii_rating = true 
+            AND end_datetime between '2021-09-01' and now()
+    '''
+    tournaments = pd.read_sql(tournaments_query, connection).get('id').tolist()
+    log.info('Got %d tournaments', len(tournaments))
+    return tournaments
+
+
+def ndcg_for_tournament(tournament_id, rating, baseline) -> dict:
+    log.info('Loading tournament %d', tournament_id)
+    row = {'tid': tournament_id}
+    for name, rating in {'rating': rating, 'baseline': baseline}.items():
+        log.info(f'Computing nDCG for {name} {rating} for tournament {tournament_id}')
+        sql = f'''
+            SELECT t.team_id, t.total, t.position, rt.mp 
+            FROM {rating}.tournament_result rt 
+            LEFT JOIN tournament_results t 
+                ON rt.tournament_id = t.tournament_id AND rt.team_id = t.team_id 
+            WHERE t.tournament_id = {tournament_id} 
+        '''
+        log.debug('SQL: %s', sql)
+        df = pd.read_sql(sql, conn)
+        # некоторые турниры могут быть без результатов
+        if df.size == 0:
+            log.error(f"no results for {tournament_id} in {rating}")
+            continue
+        y_true = df.get('position').to_numpy()
+        y_score = df.get('mp').to_numpy()
+        try:
+            ndcg = metrics.ndcg_score([y_true], [y_score])
+            log.debug('nDCG: %f', ndcg)
+            row[name] = ndcg
+        except Exception as e:
+            log.error(e)
+            log.debug(df)
+    return row
+
+
 # Парсер аргументов командной строки
-parser = argparse.ArgumentParser(description = 'Compute quality for specified (or all) tournaments')
+parser = argparse.ArgumentParser(description='Compute quality for specified (or all) tournaments')
 parser.add_argument('tournaments', metavar='TID', type=int, nargs='*',
-                    help = 'tournament identifier')
+                    help='tournament identifier')
 parser.add_argument('--rating', required=True,
-                    help = 'rating to evaluate')
+                    help='rating to evaluate')
 parser.add_argument('--baseline', default='b',
-                    help = 'baseline rating to compare to')
+                    help='baseline rating to compare to')
 
 args = parser.parse_args()
 
@@ -25,53 +76,21 @@ args = parser.parse_args()
 log.basicConfig(format='{asctime} | {levelname:8s} | {message}', style='{', level=log.INFO)
 warnings.filterwarnings('ignore', category=UserWarning)
 
-# Подключение к БД
-conn_str = os.environ.get('DATABASE')
-if conn_str is None:
-    raise Exception('DATABASE is not defined')
-
-conn = psycopg2.connect(conn_str)
-
-# Получение списка турниров
-tournaments = args.tournaments
-if tournaments == []:
-    log.info('Get all tournaments')
-    tournaments = pd.read_sql("SELECT id FROM tournaments WHERE maii_rating AND end_datetime > '2022-01-01'", conn).get('id').tolist()
-    log.info('Got %d tournaments', len(tournaments))
+conn = db_connection()
+tournaments = args.tournaments or fetch_tournaments(conn)
 
 # Получение набора пар nDCG
-results = list()
-for tid in tournaments:
-    log.info('Loading tournament %d', tid)
-    row = {'tid': tid}
-    for name, rating in {'rating': args.rating, 'baseline': args.baseline}.items():
-        log.info('Computing nDCG for %s %s for tournament %d', name, rating, tid)
-        sql = 'SELECT t.team_id, t.total, t.position, rt.mp FROM {r}.tournament_result rt LEFT JOIN tournament_results t ON rt.tournament_id = t.tournament_id AND rt.team_id = t.team_id WHERE t.tournament_id = {t}'.format(t = tid, r = rating)
-        log.debug('SQL: %s', sql)
-        df = pd.read_sql(sql, conn)
-        # некоторые турниры могут быть без результатов
-        if df.size > 0:
-            y_true = df.get('position').to_numpy()
-            y_score = df.get('mp').to_numpy()
-            try:
-                ndcg = metrics.ndcg_score([y_true], [y_score])
-                log.debug('nDCG: %f', ndcg)
-                row[name] = ndcg
-            except Exception as e:
-                log.warning(e)
-                log.debug(df)
-                pass
-    log.debug(row)
-    results.append(row)
+results = [ndcg for tid in tournaments if (ndcg := ndcg_for_tournament(tid, args.rating, args.baseline))]
 
 # Вывод финального набора пар nDCG
-res_df = pd.DataFrame(results, columns = ['tid', 'rating', 'baseline'])
-print(res_df.query('(rating >= 0) & (baseline >= 0)'))
+res_df = pd.DataFrame(results, columns=['tid', 'rating', 'baseline'])
+print(res_df.query('(rating >= 0) & (baseline >= 0)').sort_values(by='rating'))
 
 # Проверяем: альтернатива d = x - y > 0, т.е. новый рейтинг лучше старого
-w = stats.wilcoxon(x = res_df.get('rating'), y = res_df.get('baseline'),
-                     alternative = 'greater',
-                     nan_policy = 'omit')
+w = stats.wilcoxon(x=res_df.get('rating'),
+                   y=res_df.get('baseline'),
+                   alternative='greater',
+                   nan_policy='omit')
 
 # Вывод результатов
 print()
@@ -82,3 +101,6 @@ print()
 print('Medians:')
 print('new:\t', res_df.get('rating').median())
 print('old:\t', res_df.get('baseline').median())
+print('Means:')
+print('new:\t', res_df.get('rating').mean())
+print('old:\t', res_df.get('baseline').mean())
